173db494f0224df62feb3e9806eb14471646121a
Carlos Cechinel <cadu_cechinel@hotmail.com>
2024-05-20 15:02:57 -0300

    criacao exemplo

---
+++ clinica/src/tela/telaPrincipal.java
@@ -0,0 +1,8 @@
+package tela;
+
+//tudo comeca a partir de um MAIN
+public class telaPrincipal {
+    public static void main(String[] args) {
+        System.out.println("Tela Principal");
+    }
+}
